d622fac9813f81a4f3a514244be9795399da3fd3
Otávio <otaviosenun@gmail.com>
2022-02-16 15:29:47 +0000

    Added Upload page and small changes

--- UI/Pages/ComponentAPI.cs
+++ UI/Pages/ComponentAPI.cs
@@ -4,7 +4,7 @@ namespace UI.Pages;
 
 public abstract class ComponentAPI : ComponentBase
 {
-    [Inject] private IHttpClientFactory _httpFactory { get; set; }
+    [Inject] IHttpClientFactory _httpFactory { get; set; }
 
     protected HttpClient GetClient() => _httpFactory.CreateClient("api");
 }
