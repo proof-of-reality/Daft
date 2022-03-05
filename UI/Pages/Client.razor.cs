73949e4f878ead702f546d4c270e6dec241cf120
Otávio <otaviosenun@gmail.com>
2022-03-05 22:57:35 +0000

    Moved ClientDTO to code-behind file

---
+++ UI/Pages/Client.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UI.Pages;
+
+class ClientDTO : Core.Models.Client
+{
+
+    public ClientDTO() : this(string.Empty, string.Empty, string.Empty, string.Empty)
+    {
+    }
+
+    public ClientDTO(string firstName, string lastName, string email, string pass) : base(firstName, lastName, email, pass)
+    {
+    }
+
+    [Compare("Password", ErrorMessage = "Password and confirm password do not match.")]
+    public string Confirmation { get; set; }
+}
