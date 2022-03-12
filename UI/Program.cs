f0d91d1f2d7a92e3720098c555d50c89270712ec
Otávio <otaviosenun@gmail.com>
2022-03-12 00:01:45 +0000

    Increased timeout

--- UI/Program.cs
+++ UI/Program.cs
@@ -11,7 +11,11 @@ builder.Services.AddRazorPages()
 builder.Services.AddBlazoredModal();
 builder.Services.AddServerSideBlazor();
 builder.Services
-    .AddHttpClient("api", config => config.BaseAddress = new Uri("https://localhost:7255/api/"))
+    .AddHttpClient("api", config =>
+    {
+        config.Timeout = TimeSpan.FromMinutes(30);
+        config.BaseAddress = new Uri("https://localhost:7255/api/");
+    })
     .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
     {
         ClientCertificateOptions = ClientCertificateOption.Manual,
