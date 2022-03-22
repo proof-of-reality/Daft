104c36bdb5ea8ed27f6b0468a1df763587fec9cf
Otávio <otaviosenun@gmail.com>
2022-03-22 11:10:27 +0000

    Improved messages feedback to the user

--- Core/Models/Requests/EmailRequest.cs
+++ Core/Models/Requests/EmailRequest.cs
@@ -10,7 +10,7 @@ public class EmailRequest
     [Required, EmailAddress]
     public string EmailTo { get; set; } = string.Empty;
 
-    [Required, MinLength(10)]
+    [Required(ErrorMessage = "This field is required."), MinLength(10, ErrorMessage = "Please provide a more descriptive message")]
     public string Text { get; set; } = string.Empty;
 
     public long PropertyId { get; set; }
